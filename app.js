11dfbc3c6116fb1d4fd615b112f935689d2cfaae
Mat Wilmot <matwilmot90@gmail.com>
2020-04-15 16:37:15 -0700

    Added arrays for char types and input box for pw length

---
+++ app.js
@@ -0,0 +1,92 @@
+var lowerCase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var upperCase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var specialCharacters = [
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "-",
+  "_",
+  "+",
+  "=",
+  "<",
+  ">",
+];
+
+// create an html form to collect information about password content
+// see index.html
+
+// create arrays that contain the characters for the password
+// if uppercase is true, then add uppercase letters
+// if lowercase is true, then add lowercase letters
+// if numbers is true, then add numbers
+// if specialCharacters is true, then add special characters
+// get password-length
+// generate a password using random included character types, until password is the correct length
