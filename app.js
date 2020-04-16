c74a0a273a33594b03db0f8a80c41c6946b2e975
Mat Wilmot <matwilmot90@gmail.com>
2020-04-16 16:36:28 -0700

    password generator is functional, prints to console.log currently

--- app.js
+++ app.js
@@ -1,85 +1,8 @@
 // create arrays that contain the characters for the password
-var lowerCase = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-];
-var upperCase = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-];
-var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-var specialCharacters = [
-  "!",
-  "@",
-  "#",
-  "$",
-  "%",
-  "^",
-  "&",
-  "*",
-  "(",
-  ")",
-  "-",
-  "_",
-  "+",
-  "=",
-  "<",
-  ">",
-];
+var lowerCase = ["abcdefghijklmnopqrstuvwxyz"];
+var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var numbers = ["1234567890"];
+var specialCharacters = ["!@#$%^&*()_+-=?><;"];
 
 // create an html form to collect information about password content
 // see index.html
@@ -87,7 +10,7 @@ var specialCharacters = [
 
 var pwdContent = "";
 
-// get password-length, this should be inside the event listener for clicking the submit button
+// everything happens when the submit button is clicked
 document.getElementById("submit").addEventListener("click", function (e) {
   e.preventDefault();
   var wantsLowercase = document.getElementById("lowercase").checked;
@@ -95,34 +18,40 @@ document.getElementById("submit").addEventListener("click", function (e) {
   var wantsNumbers = document.getElementById("numbers").checked;
   var wantsSpecial = document.getElementById("special-characters").checked;
   var pwdLength = document.getElementById("password-length").value;
+
   if (pwdLength < 8 || pwdLength > 128) {
     console.log(pwdLength);
     alert("Password length not acceptable. Please choose between 8 and 128.");
-  }
-  if (
+  } else if (
     wantsLowercase === false &&
     wantsUppercase === false &&
     wantsNumbers === false &&
     wantsSpecial === false
   ) {
     alert("Password must contain at least one type of character");
+  } else {
+    if (wantsUppercase === true) {
+      pwdContent += lowerCase;
+    }
+    if (wantsUppercase === true) {
+      pwdContent += upperCase;
+    }
+    if (wantsNumbers === true) {
+      pwdContent += numbers;
+    }
+    if (wantsSpecial === true) {
+      pwdContent += specialCharacters;
+    }
+    console.log("password contains", pwdContent);
+    // use a for loop to pick a random character from pwdContent for pwdLength number of times
+    var newPassword = "";
+    for (var i = 0; i < pwdLength; i++) {
+      var randPwdChar = Math.round(Math.random() * pwdContent.length);
+      newPassword += pwdContent[randPwdChar];
+      console.log(pwdContent[randPwdChar]);
+    }
+    console.log(newPassword);
   }
-
-  if (wantsUppercase === true) {
-    pwdContent += lowerCase;
-  }
-  if (wantsUppercase === true) {
-    pwdContent += upperCase;
-  }
-  if (wantsNumbers === true) {
-    pwdContent += numbers;
-  }
-  if (wantsSpecial === true) {
-    pwdContent += specialCharacters;
-  }
-  console.log(pwdContent);
-
-  return pwdContent;
 });
 
 // if uppercase is true, then add uppercase letters
@@ -132,18 +61,18 @@ document.getElementById("submit").addEventListener("click", function (e) {
 // if the user selected no inputs, give an error
 // generate a password using random included character types, until password is the correct length
 
-// access and print a random lower character
-var randLC = Math.round(Math.random() * lowerCase.length);
-console.log(lowerCase[randLC]);
+// // access and print a random lower character
+// var randLC = Math.round(Math.random() * lowerCase.length);
+// console.log(lowerCase[randLC]);
 
-// access and print a random upper character
-var randUC = Math.round(Math.random() * upperCase.length);
-console.log(lowerCase[randLC]);
+// // access and print a random upper character
+// var randUC = Math.round(Math.random() * upperCase.length);
+// console.log(upperCase[randUC]);
 
-// access and print a random number
-var randNum = Math.round(Math.random() * numbers.length);
-console.log(numbers[randNum]);
+// // access and print a random number
+// var randNum = Math.round(Math.random() * numbers.length);
+// console.log(numbers[randNum]);
 
-// access and print a random special character
-var randSC = Math.round(Math.random() * specialCharacters.length);
-console.log(specialCharacters[randSC]);
+// // access and print a random special character
+// var randSC = Math.round(Math.random() * specialCharacters.length);
+// console.log(specialCharacters[randSC]);
