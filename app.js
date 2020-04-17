61f6b2fbb4190e28d114c6e43b9dac7473626909
Mat Wilmot <matwilmot90@gmail.com>
2020-04-16 17:11:35 -0700

    Added finishing touches to HTML and JS. Cleaned up comments

--- app.js
+++ app.js
@@ -4,10 +4,6 @@ var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
 var numbers = ["1234567890"];
 var specialCharacters = ["!@#$%^&*()_+-=?><;"];
 
-// create an html form to collect information about password content
-// see index.html
-// collect that true/false from each checkbox and make it a variable
-
 // everything happens when the submit button is clicked
 document.getElementById("submit").addEventListener("click", function (e) {
   e.preventDefault();
@@ -18,6 +14,7 @@ document.getElementById("submit").addEventListener("click", function (e) {
   var pwdLength = document.getElementById("password-length").value;
   var pwdContent = "";
 
+  // use conditional to determine whether or not to run the password generation
   if (pwdLength < 8 || pwdLength > 128) {
     console.log(pwdLength);
     alert("Password length not acceptable. Please choose between 8 and 128.");
@@ -29,6 +26,7 @@ document.getElementById("submit").addEventListener("click", function (e) {
   ) {
     alert("Password must contain at least one type of character");
   } else {
+    // use 4 conditionals to add arrays to empty array for final password to be chosen from
     if (wantsLowercase === true) {
       pwdContent += lowerCase;
     }
@@ -41,37 +39,17 @@ document.getElementById("submit").addEventListener("click", function (e) {
     if (wantsSpecial === true) {
       pwdContent += specialCharacters;
     }
-    console.log("password contains", pwdContent);
-    // use a for loop to pick a random character from pwdContent for pwdLength number of times
+
+    // use a for loop to pick a random character from pwdContent for pwdLength number of
+    // times and add it to newPassword
     var newPassword = "";
     for (var i = 0; i < pwdLength; i++) {
       var randPwdChar = Math.floor(Math.random() * pwdContent.length);
       newPassword += pwdContent[randPwdChar];
-      console.log(pwdContent[randPwdChar]);
-    } // end of for loop
-    console.log(newPassword);
-  } // end of else
-}); // end of event listener
+    }
 
-// if uppercase is true, then add uppercase letters
-// if lowercase is true, then add lowercase letters
-// if numbers is true, then add numbers
-// if specialCharacters is true, then add special characters
-// if the user selected no inputs, give an error
-// generate a password using random included character types, until password is the correct length
-
-// // access and print a random lower character
-// var randLC = Math.round(Math.random() * lowerCase.length);
-// console.log(lowerCase[randLC]);
-
-// // access and print a random upper character
-// var randUC = Math.round(Math.random() * upperCase.length);
-// console.log(upperCase[randUC]);
-
-// // access and print a random number
-// var randNum = Math.round(Math.random() * numbers.length);
-// console.log(numbers[randNum]);
-
-// // access and print a random special character
-// var randSC = Math.round(Math.random() * specialCharacters.length);
-// console.log(specialCharacters[randSC]);
+    // print the password to an html element.
+    document.getElementById("password").innerHTML = newPassword;
+    document.getElementById("password-length").value = "";
+  }
+});
